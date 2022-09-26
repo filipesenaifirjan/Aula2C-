799934beb73f12d045ff253389772201ddc5f943
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-26 14:56:37 -0300

    if e switch

--- Program.cs
+++ Program.cs
@@ -6,19 +6,57 @@ namespace Aula02
     {
         static void Main(string[] args)
         {   
-            int idade;
-            Console.WriteLine("Digite sua idade: ");
-            idade = int.Parse(Console.ReadLine());
+              int mes;
 
-            if( idade >=18)
-            { Console.WriteLine("Você pode ser preso kkkk");
+            Console.WriteLine("Digite um número do mês");
+            mes = int.Parse(Console.ReadLine());
 
+            switch(mes){
+                case 1:
+                Console.WriteLine("Janeiro");
+                break;
+                case 2: 
+                Console.WriteLine("feveiro");
+                break;
+                case 3: 
+                Console.WriteLine("março");
+                break;
+                case 4: 
+                Console.WriteLine("abril");
+                break;
+                case 5: 
+                Console.WriteLine("maio");
+                break;
+                case 6: 
+                Console.WriteLine("junho");
+                break;
+                case 7: 
+                Console.WriteLine("julho");
+                break;
+                case 8: 
+                Console.WriteLine("agosto");
+                break;
+                  case 9: 
+                Console.WriteLine("setembro");
+                break;
+                  case 10: 
+                Console.WriteLine("outubro");
+                break;
+                  case 11: 
+                Console.WriteLine("novembro");
+                break;
+                case 12: 
+                Console.WriteLine("dezembro");
+                break;
 
+                default:
+                Console.WriteLine("mês inválido");
+                break;
             }
-        else { 
-            Console.WriteLine("Você é menor de idade");
+
+           
+            
         }
-        }
-        
     }
 }
+//ou usa o switch case, que fica bem mais organizado 
