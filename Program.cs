7a59f94b468b0531d40dc2c1ad7e7e010c39bf28
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-26 14:15:56 -0300

    comandos-funcionais

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int idade;
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if( idade >=18)
+            { Console.WriteLine("Você pode ser preso kkkk");
+
+
+            }
+
+        }
+        
+    }
+}
